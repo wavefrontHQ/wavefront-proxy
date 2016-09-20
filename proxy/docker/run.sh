4637a03f8d9bc9fbedd6d02d9591d4f9c48b1696
Mori Bellamy <mori@wavefront.com>
2016-09-20 16:01:41 -0700

    export HOSTNAME explicitly in run.sh. better logging too

--- proxy/docker/run.sh
+++ proxy/docker/run.sh
@@ -4,10 +4,13 @@ set -x
 spool_dir="/var/spool/wavefront-proxy"
 mkdir -p $spool_dir
 
+WAVEFRONT_HOSTNAME=${WAVEFRONT_HOSTNAME:-$(hostname)}
+export WAVEFRONT_HOSTNAME
+
 # HACK, we can remove this sed once we release 3.25, which honors DO_SERVICE_RESTART.
 autoconf=/opt/wavefront/wavefront-proxy/bin/autoconf-wavefront-proxy.sh
 sed -i"" "/init\.d/d" $autoconf
-$autoconf
+/bin/bash -x $autoconf
 
 logfile=/var/log/wavefront.log
 java_heap_usage=${JAVA_HEAP_USAGE:-4G}
