a825af3d4d289dd0f14120f4c4d8af7e21db3f7c
basilisk487 <vasily@wavefront.com>
2016-09-21 08:40:26 -0500

    Update as per code review

--- proxy/src/main/java/com/wavefront/agent/preprocessor/AgentPreprocessorConfiguration.java
+++ proxy/src/main/java/com/wavefront/agent/preprocessor/AgentPreprocessorConfiguration.java
@@ -33,95 +33,96 @@ public class AgentPreprocessorConfiguration {
 
   public void loadFromStream(InputStream stream) {
     Yaml yaml = new Yaml();
-    Map<String, Map<String, Map<String, String>>> rulesByPort;
+    Map<String, Object> rulesByPort;
     try {
       //noinspection unchecked
-      rulesByPort = (Map<String, Map<String, Map<String, String>>>) yaml.load(stream);
+      rulesByPort = (Map<String, Object>) yaml.load(stream);
+      int totalValidRules = 0;
+      for (String strPort : rulesByPort.keySet()) {
+        int validRules = 0;
+        try {
+          int dummy = Integer.parseInt(strPort);
+        } catch (NumberFormatException ex) {
+          continue; // only load rules for numeric ports
+        }
+        //noinspection unchecked
+        Map<String, Object> rules = (Map<String, Object>) rulesByPort.get(strPort);
+        for (String ruleKey : rules.keySet()) {
+          //noinspection unchecked
+          Map<String, String> rule = (Map<String, String>) rules.get(ruleKey);
+          try {
+            Counter counter = Metrics.newCounter(
+                new TaggedMetricName("preprocessor." + ruleKey, "count", "port", strPort));
+            if (rule.get("scope") != null && rule.get("scope").equals("pointLine")) {
+              switch (rule.get("action")) {
+                case "replaceRegex":
+                  this.forPort(strPort).forPointLine().addTransformer(
+                      new PointLineReplaceRegexTransformer(rule.get("search"), rule.get("replace"), counter));
+                  validRules++;
+                  break;
+                case "blacklistRegex":
+                  this.forPort(strPort).forPointLine().addFilter(
+                      new PointLineBlacklistRegexFilter(rule.get("match"), counter));
+                  validRules++;
+                  break;
+                case "whitelistRegex":
+                  this.forPort(strPort).forPointLine().addFilter(
+                      new PointLineWhitelistRegexFilter(rule.get("match"), counter));
+                  validRules++;
+                  break;
+              }
+            } else {
+              switch (rule.get("action")) {
+                case "replaceRegex":
+                  this.forPort(strPort).forReportPoint().addTransformer(
+                      new ReportPointReplaceRegexTransformer(
+                          rule.get("scope"), rule.get("search"), rule.get("replace"), counter));
+                  validRules++;
+                  break;
+                case "addTag":
+                  this.forPort(strPort).forReportPoint().addTransformer(
+                      new ReportPointAddTagTransformer(rule.get("tag"), rule.get("value"), counter));
+                  validRules++;
+                  break;
+                case "addTagIfNotExists":
+                  this.forPort(strPort).forReportPoint().addTransformer(
+                      new ReportPointAddTagIfNotExistsTransformer(rule.get("tag"), rule.get("value"), counter));
+                  validRules++;
+                  break;
+                case "dropTag":
+                  this.forPort(strPort).forReportPoint().addTransformer(
+                      new ReportPointDropTagTransformer(rule.get("tag"), rule.get("match"), counter));
+                  validRules++;
+                  break;
+                case "renameTag":
+                  this.forPort(strPort).forReportPoint().addTransformer(
+                      new ReportPointRenameTagTransformer(
+                          rule.get("tag"), rule.get("newtag"), rule.get("match"), counter));
+                  validRules++;
+                  break;
+                case "blacklistRegex":
+                  this.forPort(strPort).forReportPoint().addFilter(
+                      new ReportPointBlacklistRegexFilter(rule.get("scope"), rule.get("match"), counter));
+                  validRules++;
+                  break;
+                case "whitelistRegex":
+                  this.forPort(strPort).forReportPoint().addFilter(
+                      new ReportPointWhitelistRegexFilter(rule.get("scope"), rule.get("match"), counter));
+                  validRules++;
+                  break;
+              }
+            }
+          } catch (IllegalArgumentException | NullPointerException ex) {
+            logger.warning("Unable to load rule " + ruleKey + " (port " + strPort + "): " + ex);
+          }
+        }
+        logger.info("Loaded " + validRules + " rules for port " + strPort);
+        totalValidRules += validRules;
+      }
+      logger.info("Total " + totalValidRules + " rules loaded");
     } catch (ClassCastException e) {
       logger.warning("Can't parse preprocessor configuration");
       throw e;
     }
-
-    int totalValidRules = 0;
-    for (String strPort : rulesByPort.keySet()) {
-      int validRules = 0;
-      try {
-        Integer.parseInt(strPort);
-      } catch (NumberFormatException ex) {
-        continue; // only load rules for numeric ports
-      }
-      Map<String, Map<String, String>> rules = rulesByPort.get(strPort);
-      for (String ruleKey: rules.keySet()) {
-        Map<String, String> rule = rules.get(ruleKey);
-        try {
-          Counter counter = Metrics.newCounter(
-              new TaggedMetricName("preprocessor." + ruleKey, "count", "port", strPort));
-          if (rule.get("scope") != null && rule.get("scope").equals("pointLine")) {
-            switch (rule.get("action")) {
-              case "replaceRegex":
-                this.forPort(strPort).forPointLine().addTransformer(
-                    new PointLineReplaceRegexTransformer(rule.get("search"), rule.get("replace"), counter));
-                validRules++;
-                break;
-              case "blacklistRegex":
-                this.forPort(strPort).forPointLine().addFilter(
-                    new PointLineBlacklistRegexFilter(rule.get("match"), counter));
-                validRules++;
-                break;
-              case "whitelistRegex":
-                this.forPort(strPort).forPointLine().addFilter(
-                    new PointLineWhitelistRegexFilter(rule.get("match"), counter));
-                validRules++;
-                break;
-            }
-          } else {
-            switch (rule.get("action")) {
-              case "replaceRegex":
-                this.forPort(strPort).forReportPoint().addTransformer(
-                    new ReportPointReplaceRegexTransformer(
-                        rule.get("scope"), rule.get("search"), rule.get("replace"), counter));
-                validRules++;
-                break;
-              case "addTag":
-                this.forPort(strPort).forReportPoint().addTransformer(
-                    new ReportPointAddTagTransformer(rule.get("tag"), rule.get("value"), counter));
-                validRules++;
-                break;
-              case "addTagIfNotExists":
-                this.forPort(strPort).forReportPoint().addTransformer(
-                    new ReportPointAddTagIfNotExistsTransformer(rule.get("tag"), rule.get("value"), counter));
-                validRules++;
-                break;
-              case "dropTag":
-                this.forPort(strPort).forReportPoint().addTransformer(
-                    new ReportPointDropTagTransformer(rule.get("tag"), rule.get("match"), counter));
-                validRules++;
-                break;
-              case "renameTag":
-                this.forPort(strPort).forReportPoint().addTransformer(
-                    new ReportPointRenameTagTransformer(
-                        rule.get("tag"), rule.get("newtag"), rule.get("match"), counter));
-                validRules++;
-                break;
-              case "blacklistRegex":
-                this.forPort(strPort).forReportPoint().addFilter(
-                    new ReportPointBlacklistRegexFilter(rule.get("scope"), rule.get("match"), counter));
-                validRules++;
-                break;
-              case "whitelistRegex":
-                this.forPort(strPort).forReportPoint().addFilter(
-                    new ReportPointWhitelistRegexFilter(rule.get("scope"), rule.get("match"), counter));
-                validRules++;
-                break;
-            }
-          }
-        } catch (IllegalArgumentException | NullPointerException ex) {
-          logger.warning("Unable to load rule " + ruleKey + " (port " + strPort + "): " + ex);
-        }
-      }
-      logger.info("Loaded " + validRules + " rules for port " + strPort);
-      totalValidRules += validRules;
-    }
-    logger.info("Total " + totalValidRules + " rules loaded");
   }
 }
