3cf87da71eef30b22142ec4a355bab418a1e3c48
Evan Pease <evanpease@WFSE01MBPRO.local>
2016-09-21 10:59:41 -0700

    removed comment

--- proxy/docker/run.sh
+++ proxy/docker/run.sh
@@ -12,11 +12,8 @@ autoconf=/opt/wavefront/wavefront-proxy/bin/autoconf-wavefront-proxy.sh
 sed -i"" "/init\.d/d" $autoconf
 /bin/bash -x $autoconf
 
-#logfile=/var/log/wavefront.log
 java_heap_usage=${JAVA_HEAP_USAGE:-4G}
 java \
 	-Xmx$java_heap_usage -Xms$java_heap_usage \
 	-jar /opt/wavefront/wavefront-proxy/bin/wavefront-push-agent.jar \
 	-f /opt/wavefront/wavefront-proxy/conf/wavefront.conf
-
-#tail -f $logfile
