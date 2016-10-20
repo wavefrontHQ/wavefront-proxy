d70900f1a0280bd2059a2cdf1cb866cc1a6586fb
Mori Bellamy <mori@wavefront.com>
2016-10-20 14:56:46 -0700

    improve javadoc

---
+++ proxy/src/main/java/com/wavefront/agent/logsharvesting/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes for harvesting metric data from logs and sending them to Wavefront.
+ *
+ * @see com.wavefront.agent.config.LogsIngestionConfig
+ * @author Mori Bellamy (mori@wavefront.com)
+ */
+package com.wavefront.agent.logsharvesting;
