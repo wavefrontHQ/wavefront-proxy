416f500c1dbeb2a625775d4b52eacda6b4464376
basilisk487 <vasily@wavefront.com>
2016-10-20 20:00:58 -0500

    Revert condition to handle .deb files properly

--- pkg/before-remove.sh
+++ pkg/before-remove.sh
@@ -8,8 +8,12 @@ service wavefront-proxy stop || true
 
 # rpm passes "0", "1" or "2" as a command line argument - due to the order in which scripts are executed by rpm
 # (post-install for new version first and only then before-remove for the old version), we can only safely delete
-# the JRE that we downloaded if the argument is "0", meaning that it's an uninstall.
-if [[ "$1" == "0" ]]; then
+# the JRE that we downloaded if the argument is "0", meaning that it's an uninstall
+#
+# Ref:
+#  - http://www.rpm.org/max-rpm/s1-rpm-inside-scripts.html
+#  - https://www.debian.org/doc/debian-policy/ch-maintainerscripts.html
+if [[ "$1" == "0" ]] || [[ "$1" == "remove" ]] || [[ "$1" == "purge" ]]; then
     echo "Removing installed JRE from $jre_dir"
     rm -rf $jre_dir
 fi
