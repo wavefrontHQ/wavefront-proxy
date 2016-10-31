0e685d770bac463484a7f663b5c69067919aaf55
Vasily Vorontsov <vasily@wavefront.com>
2016-10-31 14:08:52 -0500

    Switch retry queue to LZ4 compression with GZIP backwards compatibility (#113)
    * Switch to LZ4 compression with GZIP backwards compatibility
    
    * formatting

--- proxy/src/main/java/com/wavefront/agent/ResubmissionTask.java
+++ proxy/src/main/java/com/wavefront/agent/ResubmissionTask.java
@@ -3,6 +3,7 @@ package com.wavefront.agent;
 import com.squareup.tape.Task;
 import com.wavefront.api.AgentAPI;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,17 +12,17 @@ import java.util.UUID;
  *
  * @author Clement Pang (clement@wavefront.com).
  */
-public abstract class ResubmissionTask<T extends ResubmissionTask<T>> implements Task {
+public abstract class ResubmissionTask<T extends ResubmissionTask<T>> implements Task, Serializable {
 
   /**
    * To be injected. Should be null when serialized.
    */
-  protected AgentAPI service = null;
+  protected transient AgentAPI service = null;
 
   /**
    * To be injected. Should be null when serialized.
    */
-  protected UUID currentAgentId = null;
+  protected transient UUID currentAgentId = null;
 
   public abstract List<T> splitTask();
 }
