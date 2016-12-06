40c80cd8fee357eeea0d6d1b99a63b05e96e124a
basilisk487 <vasily@wavefront.com>
2016-12-06 12:38:54 -0600

    Don't increment limiter metrics when not rate limiting

--- proxy/src/main/java/com/wavefront/agent/PostPushDataTimedTask.java
+++ proxy/src/main/java/com/wavefront/agent/PostPushDataTimedTask.java
@@ -262,7 +262,9 @@ public class PostPushDataTimedTask implements Runnable {
           // update the counters as if this was a failed call to the API
           this.pointsAttempted.inc(pushDataPointCount);
           this.pointsQueued.inc(pushDataPointCount);
-          this.permitsDenied.inc(pushDataPointCount);
+          if (pushRateLimiter != null) {
+            this.permitsDenied.inc(pushDataPointCount);
+          }
           numApiCalls++;
           pointsToFlush -= pushDataPointCount;
         } else {
