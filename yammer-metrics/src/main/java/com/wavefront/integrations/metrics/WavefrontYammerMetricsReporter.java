5976ff3981d9ab44907b959b8cf79d0cf53c2511
Mori Bellamy <mori@wavefront.com>
2017-02-22 18:37:10 -0800

    pluggable transformation for metricNames for WavefrontYammerMetricsReporter (#154)

--- yammer-metrics/src/main/java/com/wavefront/integrations/metrics/WavefrontYammerMetricsReporter.java
+++ yammer-metrics/src/main/java/com/wavefront/integrations/metrics/WavefrontYammerMetricsReporter.java
@@ -8,9 +8,12 @@ import com.yammer.metrics.reporting.AbstractPollingReporter;
 import java.io.IOException;
 import java.util.Map;
 import java.util.SortedMap;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.annotation.Nullable;
 
 /**
  * @author Mori Bellamy (mori@wavefront.com)
@@ -19,16 +22,34 @@ public class WavefrontYammerMetricsReporter extends AbstractPollingReporter {
 
   protected static final Logger logger = Logger.getLogger(WavefrontYammerMetricsReporter.class.getCanonicalName());
   private SocketMetricsProcessor socketMetricProcessor;
+  private Function<MetricName, MetricName> transformer;
 
   public WavefrontYammerMetricsReporter(MetricsRegistry metricsRegistry, String name, String hostname, int port,
                                         int wavefrontHistogramPort, Supplier<Long> timeSupplier) throws IOException {
-    this(metricsRegistry, name, hostname, port, wavefrontHistogramPort, timeSupplier, false);
+    this(metricsRegistry, name, hostname, port, wavefrontHistogramPort, timeSupplier, false, null);
   }
 
+  /**
+   * Reporter of a Yammer metrics registry to Wavefront
+   * @param metricsRegistry The registry to scan-and-report
+   * @param name A human readable name for this reporter
+   * @param hostname The remote host where the wavefront proxy resides
+   * @param port Listening port on Wavefront proxy of graphite-like telemetry data
+   * @param wavefrontHistogramPort Listening port for Wavefront histogram data
+   * @param timeSupplier Get current timestamp, stubbed for testing
+   * @param prependGroupName If true, outgoing telemetry is of the form "group.name" rather than "name".
+   * @param transformer If present, applied to each MetricName before flushing to Wavefront. This is useful for adding
+   *                    point tags. Warning: this is called once per metric per scan, so it should probably be
+   *                    performant.
+   * @throws IOException When we can't remotely connect to Wavefront.
+   */
   public WavefrontYammerMetricsReporter(MetricsRegistry metricsRegistry, String name, String hostname, int port,
                                         int wavefrontHistogramPort, Supplier<Long> timeSupplier,
-                                        boolean prependGroupName) throws IOException {
+                                        boolean prependGroupName,
+                                        @Nullable Function<MetricName, MetricName> transformer)
+      throws IOException {
     super(metricsRegistry, name);
+    this.transformer = transformer;
     this.socketMetricProcessor = new SocketMetricsProcessor(hostname, port, wavefrontHistogramPort, timeSupplier,
         prependGroupName);
   }
@@ -38,7 +59,11 @@ public class WavefrontYammerMetricsReporter extends AbstractPollingReporter {
     try {
       for (Map.Entry<String, SortedMap<MetricName, Metric>> entry : getMetricsRegistry().groupedMetrics().entrySet()) {
         for (Map.Entry<MetricName, Metric> subEntry : entry.getValue().entrySet()) {
-          subEntry.getValue().processWith(socketMetricProcessor, subEntry.getKey(), null);
+          MetricName metricName = subEntry.getKey();
+          if (transformer != null) {
+            metricName = transformer.apply(metricName);
+          }
+          subEntry.getValue().processWith(socketMetricProcessor, metricName, null);
         }
       }
       socketMetricProcessor.flush();
