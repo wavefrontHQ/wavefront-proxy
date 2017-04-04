c7669c86cd14c065fd0ca52642630dabe1bd76c6
basilisk487 <vasily@wavefront.com>
2017-04-04 18:57:09 -0500

    Track proxy config settings as metrics

---
+++ proxy/src/main/java/com/wavefront/agent/config/ReportableConfig.java
@@ -0,0 +1,81 @@
+package com.wavefront.agent.config;
+
+import com.wavefront.common.TaggedMetricName;
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Gauge;
+import com.yammer.metrics.core.MetricName;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.function.Function;
+
+import javax.annotation.Nullable;
+
+/**
+ * Wrapper class to simplify access to .properties file + track settings as metrics as they are retrieved
+ *
+ * @author vasily@wavefront.com
+ */
+public class ReportableConfiguration {
+  private Properties prop = new Properties();
+
+  public ReportableConfiguration(InputStream stream) throws IOException {
+    prop.load(stream);
+  }
+
+  public ReportableConfiguration(String fileName) throws IOException {
+    prop.load(new FileInputStream(fileName));
+  }
+
+  /**
+   * Returns string value for the property without tracking it as a metric
+   *
+   */
+  public String getPropertyRaw(String key, String defaultValue) {
+    return prop.getProperty(key, defaultValue);
+  }
+
+  public Number getPropertyAsNumber(String key, Number defaultValue) {
+    Long l = Long.parseLong(prop.getProperty(key, String.valueOf(defaultValue)).trim());
+    reportGauge(l, new MetricName("config", "", key));
+    return l;
+  }
+
+  public String getPropertyAsString(String key, String defaultValue) {
+    return getPropertyAsString(key, defaultValue, null);
+  }
+
+  public String getPropertyAsString(String key, String defaultValue,
+                                    @Nullable Function<String, String> converter) {
+    String s = prop.getProperty(key, defaultValue);
+    if (s != null && !s.trim().isEmpty()) {
+      reportGauge(1, new TaggedMetricName("config", key, "value", converter == null ? s : converter.apply(s)));
+    } else {
+      reportGauge(0, new MetricName("config", "", key));
+    }
+    return s;
+  }
+
+  public Boolean getPropertyAsBoolean(String key, Boolean defaultValue) {
+    Boolean b = Boolean.parseBoolean(prop.getProperty(key, String.valueOf(defaultValue)).trim());
+    reportGauge(b ? 1 : 0, new MetricName("config", "", key));
+    return b;
+  }
+
+  public void reportSettingAsGauge(Number number, String key) {
+    reportGauge(number, new MetricName("config", "", key));
+  }
+
+  public void reportGauge(Number number, MetricName metricName) {
+    Metrics.newGauge(metricName,
+        new Gauge<Double>() {
+          @Override
+          public Double value() {
+            return number.doubleValue();
+          }
+        }
+    );
+  }
+}
