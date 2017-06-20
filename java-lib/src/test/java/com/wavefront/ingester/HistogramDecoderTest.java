056a6e17c2470a990474400fca2303b394cb5f2f
Sushant Dewan <sushant@Sushants-MacBook-Pro.local>
2017-06-19 18:28:28 -0700

    Updated test code

--- java-lib/src/test/java/com/wavefront/ingester/HistogramDecoderTest.java
+++ java-lib/src/test/java/com/wavefront/ingester/HistogramDecoderTest.java
@@ -1,5 +1,7 @@
 package com.wavefront.ingester;
 
+import com.wavefront.common.Clock;
+
 import org.apache.commons.lang.time.DateUtils;
 import org.junit.Test;
 
@@ -154,12 +156,42 @@ public class HistogramDecoderTest {
     decoder.decodeReportPoints("1471988653 #3 123.237 TestMetric source=Test tag=value", out, "customer");
   }
 
-  @Test(expected = RuntimeException.class)
+  @Test
   public void testMissingTimestamp() {
+    //Note - missingTimestamp to port 40,000 is no longer invalid - see MONIT-6430 for more details
     HistogramDecoder decoder = new HistogramDecoder();
     List<ReportPoint> out = new ArrayList<>();
 
     decoder.decodeReportPoints("!M #3 123.237 TestMetric source=Test tag=value", out, "customer");
+
+    assertThat(out).isNotEmpty();
+    long expectedTimestamp = Clock.now();
+
+    ReportPoint p = out.get(0);
+    assertThat(p.getMetric()).isEqualTo("TestMetric");
+
+    assertThat(p.getValue()).isNotNull();
+    assertThat(p.getValue().getClass()).isEqualTo(Histogram.class);
+
+    // Should be converted to Millis and pinned to the beginning of the corresponding minute
+    long duration = ((Histogram) p.getValue()).getDuration();
+    expectedTimestamp = (expectedTimestamp / duration) * duration;
+    assertThat(p.getTimestamp()).isEqualTo(expectedTimestamp);
+
+    assertThat(p.getHost()).isEqualTo("Test");
+    assertThat(p.getTable()).isEqualTo("customer");
+    assertThat(p.getAnnotations()).isNotNull();
+    assertThat(p.getAnnotations()).containsEntry("_tag", "value");
+
+    Histogram h = (Histogram) p.getValue();
+
+    assertThat(h.getDuration()).isEqualTo(DateUtils.MILLIS_PER_MINUTE);
+    assertThat(h.getBins()).isNotNull();
+    assertThat(h.getBins()).isNotEmpty();
+    assertThat(h.getBins()).containsExactly(123.237D);
+    assertThat(h.getCounts()).isNotNull();
+    assertThat(h.getCounts()).isNotEmpty();
+    assertThat(h.getCounts()).containsExactly(3);
   }
 
   @Test(expected = RuntimeException.class)
@@ -170,12 +202,43 @@ public class HistogramDecoderTest {
     decoder.decodeReportPoints("!M 1471988653 TestMetric source=Test tag=value", out, "customer");
   }
 
-  @Test(expected = RuntimeException.class)
+  @Test
   public void testMissingMean() {
+    //Note - missingTimestamp to port 40,000 is no longer invalid - see MONIT-6430 for more details
+    // as a side-effect of that, this test no longer fails!!!
     HistogramDecoder decoder = new HistogramDecoder();
     List<ReportPoint> out = new ArrayList<>();
 
     decoder.decodeReportPoints("!M #3 1471988653 TestMetric source=Test tag=value", out, "customer");
+
+    assertThat(out).isNotEmpty();
+    long expectedTimestamp = Clock.now();
+
+    ReportPoint p = out.get(0);
+    assertThat(p.getMetric()).isEqualTo("TestMetric");
+
+    assertThat(p.getValue()).isNotNull();
+    assertThat(p.getValue().getClass()).isEqualTo(Histogram.class);
+
+    // Should be converted to Millis and pinned to the beginning of the corresponding minute
+    long duration = ((Histogram) p.getValue()).getDuration();
+    expectedTimestamp = (expectedTimestamp / duration) * duration;
+    assertThat(p.getTimestamp()).isEqualTo(expectedTimestamp);
+
+    assertThat(p.getHost()).isEqualTo("Test");
+    assertThat(p.getTable()).isEqualTo("customer");
+    assertThat(p.getAnnotations()).isNotNull();
+    assertThat(p.getAnnotations()).containsEntry("_tag", "value");
+
+    Histogram h = (Histogram) p.getValue();
+
+    assertThat(h.getDuration()).isEqualTo(DateUtils.MILLIS_PER_MINUTE);
+    assertThat(h.getBins()).isNotNull();
+    assertThat(h.getBins()).isNotEmpty();
+    assertThat(h.getBins()).containsExactly(1471988653.0);
+    assertThat(h.getCounts()).isNotNull();
+    assertThat(h.getCounts()).isNotEmpty();
+    assertThat(h.getCounts()).containsExactly(3);
   }
 
   @Test(expected = RuntimeException.class)
