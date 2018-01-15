73d68e3bfd30df9ca27fc031d064bf5d9d554737
srinivas-kandula <skandula@vmare.com>
2018-01-15 07:09:37 +0530

    MONIT-7898 Added support for OpenSUSE

--- pkg/after-remove.sh
+++ pkg/after-remove.sh
@@ -5,6 +5,9 @@ if [[ -f /etc/debian_version ]]; then
 	update-rc.d -f wavefront-proxy remove
 elif [[ -f /etc/redhat-release ]] || [[ -f /etc/system-release-cpe ]]; then
 	chkconfig --del wavefront-proxy
+elif [[ -f /etc/SUSE-brand ]]; then
+        systemctl stop wavefront-proxy
+        chkconfig wavefront-proxy off
 fi
 
 exit 0
