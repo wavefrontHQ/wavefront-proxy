9e1cfaf45633af2c8f8962782e8be0d8038c62ff
howardyoo <32691630+howardyoo@users.noreply.github.com>
2018-10-02 21:38:16 -0500

    Wavefront Proxy raw data logging (#305)

--- proxy/src/main/java/com/wavefront/agent/listeners/ChannelStringHandler.java
+++ proxy/src/main/java/com/wavefront/agent/listeners/ChannelStringHandler.java
@@ -9,14 +9,21 @@ import com.wavefront.agent.preprocessor.ReportableEntityPreprocessor;
 import com.wavefront.ingester.Decoder;
 
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Nullable;
 
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+
+import org.apache.commons.lang.math.NumberUtils;
+
 import wavefront.report.ReportPoint;
 
 /**
@@ -29,8 +36,10 @@ import wavefront.report.ReportPoint;
 public class ChannelStringHandler extends SimpleChannelInboundHandler<String> {
 
   private static final Logger blockedPointsLogger = Logger.getLogger("RawBlockedPoints");
+  private static final Logger rawDataLogger = Logger.getLogger("RawDataLogger");
 
   private final Decoder<String> decoder;
+  private static final Random RANDOM = new Random();
 
   /**
    * Transformer to transform each line.
@@ -39,17 +48,64 @@ public class ChannelStringHandler extends SimpleChannelInboundHandler<String> {
   private final ReportableEntityPreprocessor preprocessor;
   private final PointHandler pointHandler;
 
+  /**
+   * Value of system property wavefront.proxy.lograwdata (for backwards compatibility)
+   */
+  private final boolean logRawDataFlag;
+  private double logRawDataRate;
+  private volatile long logRawUpdatedMillis = 0L;
+  private boolean logRawData = false;
+
   public ChannelStringHandler(Decoder<String> decoder,
                               final PointHandler pointhandler,
                               @Nullable final ReportableEntityPreprocessor preprocessor) {
     this.decoder = decoder;
     this.pointHandler = pointhandler;
     this.preprocessor = preprocessor;
+
+    // check the property setting for logging raw data
+    @Nullable String logRawDataProperty = System.getProperty("wavefront.proxy.lograwdata");
+    logRawDataFlag = logRawDataProperty != null && logRawDataProperty.equalsIgnoreCase("true");
+    @Nullable String logRawDataSampleRateProperty = System.getProperty("wavefront.proxy.lograwdata.sample-rate");
+    this.logRawDataRate = logRawDataSampleRateProperty != null &&
+        NumberUtils.isNumber(logRawDataSampleRateProperty) ? Double.parseDouble(logRawDataSampleRateProperty) : 1.0d;
+
+    // make sure the rate fits between 0.0d - 1.0d
+    if (logRawDataRate < 0.0d) {
+      rawDataLogger.info("Invalid log raw data rate:" + logRawDataRate + ", adjusted to 0.0");
+      logRawDataRate = 0.0d;
+    } else if (logRawDataRate > 1.0d) {
+      rawDataLogger.info("Invalid log raw data rate:" + logRawDataRate + ", adjusted to 1.0");
+      logRawDataRate = 1.0d;
+    }
   }
 
   @Override
   protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-    // if msg does not match metadata keywords then treat it as a point
+    // use data rate to determine sampling rate
+    // logging includes the source host and port
+    if (logRawUpdatedMillis + TimeUnit.SECONDS.toMillis(1) < System.currentTimeMillis()) {
+      if (logRawData != rawDataLogger.isLoggable(Level.FINEST)) {
+        logRawData = !logRawData;
+        rawDataLogger.info("Raw data logging is now " + (logRawData ?
+            "enabled with " + (logRawDataRate * 100) + "% sampling" :
+            "disabled"));
+      }
+      logRawUpdatedMillis = System.currentTimeMillis();
+    }
+
+    if ((logRawData || logRawDataFlag) &&
+        (logRawDataRate >= 1.0d || (logRawDataRate > 0.0d && RANDOM.nextDouble() < logRawDataRate))) {
+      if (ctx.channel().remoteAddress() != null) {
+        String hostAddress = ((InetSocketAddress) ctx.channel().remoteAddress()).getAddress().getHostAddress();
+        int localPort = ((InetSocketAddress) ctx.channel().localAddress()).getPort();
+        rawDataLogger.info("[" + hostAddress + ">" + localPort + "]" + msg);
+      } else {
+        int localPort = ((InetSocketAddress) ctx.channel().localAddress()).getPort();
+        rawDataLogger.info("[>" + localPort + "]" + msg);
+        
+      }
+    }
     processPointLine(msg, decoder, pointHandler, preprocessor, ctx);
   }
 
@@ -70,7 +126,6 @@ public class ChannelStringHandler extends SimpleChannelInboundHandler<String> {
     // transform the line if needed
     if (preprocessor != null) {
       pointLine = preprocessor.forPointLine().transform(pointLine);
-
       // apply white/black lists after formatting
       if (!preprocessor.forPointLine().filter(pointLine)) {
         if (preprocessor.forPointLine().getLastFilterResult() != null) {
