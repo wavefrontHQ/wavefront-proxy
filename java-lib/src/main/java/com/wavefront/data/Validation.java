1fc07d30d8e32d4689ece6a139b7c9ddc391c7a4
Srujan Narkedamalli <snarkedamall@vmware.com>
2018-11-07 14:07:38 -0800

    Allow metrics with tilda(~) prefix.

--- java-lib/src/main/java/com/wavefront/data/Validation.java
+++ java-lib/src/main/java/com/wavefront/data/Validation.java
@@ -40,8 +40,9 @@ public class Validation {
       char cur = input.charAt(i);
       if (!(44 <= cur && cur <= 57) && !(65 <= cur && cur <= 90) && !(97 <= cur && cur <= 122) &&
           cur != 95) {
-        if (!((i == 0 && cur == 0x2206) || (i == 0 && cur == 0x0394))) {
+        if (!((i == 0 && cur == 0x2206) || (i == 0 && cur == 0x0394) || (i == 0 && cur == 126))) {
           // first character can also be \u2206 (∆ - INCREMENT) or \u0394 (Δ - GREEK CAPITAL LETTER DELTA)
+          // or ~ tilda character for internal metrics
           return false;
         }
       }
