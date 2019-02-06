952e0ff7f0bf71d758573ad7d1175e19de6b6212
Vasily V <vasily@wavefront.com>
2019-02-06 13:03:18 -0600

    Simplify proxy chaining architecture (#351)

--- proxy/src/main/java/com/wavefront/agent/listeners/RelayPortUnificationHandler.java
+++ proxy/src/main/java/com/wavefront/agent/listeners/RelayPortUnificationHandler.java
@@ -2,8 +2,6 @@ package com.wavefront.agent.listeners;
 
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import com.wavefront.agent.auth.TokenAuthenticator;
 import com.wavefront.agent.handlers.ReportableEntityHandlerFactory;
 import com.wavefront.agent.preprocessor.ReportableEntityPreprocessor;
@@ -15,10 +13,7 @@ import org.apache.commons.lang.StringUtils;
 
 import java.net.URI;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.annotation.Nullable;
 
@@ -39,49 +34,12 @@ import io.netty.util.CharsetUtil;
 public class RelayPortUnificationHandler extends WavefrontPortUnificationHandler {
   private static final Logger logger = Logger.getLogger(RelayPortUnificationHandler.class.getCanonicalName());
 
-  private static final Pattern PATTERN_CHECKIN = Pattern.compile("/api/daemon/(.*)/checkin");
-  private static final Pattern PATTERN_PUSHDATA = Pattern.compile("/api/daemon/(.*)/pushdata/(.*)");
-
-  private Cache<String, String> proxyTokenCache = Caffeine.newBuilder()
-      .expireAfterWrite(10, TimeUnit.MINUTES)
-      .maximumSize(10_000)
-      .build();
-
   public RelayPortUnificationHandler(final String handle,
                                      final TokenAuthenticator tokenAuthenticator,
                                      final Map<ReportableEntityType, ReportableEntityDecoder> decoders,
                                      final ReportableEntityHandlerFactory handlerFactory,
                                      @Nullable final ReportableEntityPreprocessor preprocessor) {
     super(handle, tokenAuthenticator, decoders, handlerFactory, null, preprocessor);
-  }
-
-  @Override
-  protected boolean authorized(final ChannelHandlerContext ctx, final FullHttpRequest request) {
-    if (tokenAuthenticator.authRequired()) {
-      String token = extractToken(ctx, request);
-
-      URI uri = parseUri(ctx, request);
-      if (uri == null) return false;
-
-      Matcher patternPushDataMatcher = PATTERN_PUSHDATA.matcher(uri.getPath());
-      if (patternPushDataMatcher.matches()) {
-        // extract proxy ID from the URL and get actual token from cache
-        token = proxyTokenCache.getIfPresent(patternPushDataMatcher.replaceAll("$1"));
-      }
-
-      if (!tokenAuthenticator.authorize(token)) { // 401 if no token or auth fails
-        writeHttpResponse(ctx, HttpResponseStatus.UNAUTHORIZED, "401 Unauthorized\n");
-        return false;
-      }
-
-      Matcher patternCheckinMatcher = PATTERN_CHECKIN.matcher(uri.getPath());
-      if (patternCheckinMatcher.matches() && token != null) {
-        String proxyId = patternCheckinMatcher.replaceAll("$1");
-        logger.info("Caching auth token for proxy ID " + proxyId);
-        proxyTokenCache.put(proxyId, token);
-      }
-    }
-    return true;
   }
 
   @Override
