e40b6b2db3faeb3f2f13f7102b2cc3bb47348685
basilisk487 <vasily@wavefront.com>
2019-08-05 16:56:20 -0500

    Fix missing raw logs received metric

--- proxy/src/main/java/com/wavefront/agent/listeners/RawLogsIngesterPortUnificationHandler.java
+++ proxy/src/main/java/com/wavefront/agent/listeners/RawLogsIngesterPortUnificationHandler.java
@@ -6,6 +6,9 @@ import com.wavefront.agent.auth.TokenAuthenticator;
 import com.wavefront.agent.logsharvesting.LogsIngester;
 import com.wavefront.agent.logsharvesting.LogsMessage;
 import com.wavefront.agent.preprocessor.ReportableEntityPreprocessor;
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Counter;
+import com.yammer.metrics.core.MetricName;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -30,26 +33,31 @@ import static com.wavefront.agent.channel.CachingHostnameLookupResolver.getRemot
  * @author vasily@wavefront.com
  */
 public class RawLogsIngesterPortUnificationHandler extends PortUnificationHandler {
-  private static final Logger logger = Logger.getLogger(RawLogsIngesterPortUnificationHandler.class.getCanonicalName());
+  private static final Logger logger = Logger.getLogger(
+      RawLogsIngesterPortUnificationHandler.class.getCanonicalName());
 
   private final LogsIngester logsIngester;
   private final Function<InetAddress, String> hostnameResolver;
   private final Supplier<ReportableEntityPreprocessor> preprocessorSupplier;
+
+  private final Counter received = Metrics.newCounter(new MetricName("logsharvesting", "",
+      "raw-received"));
 
   /**
    * Create new instance.
    *
    * @param handle              handle/port number.
    * @param ingester            log ingester.
-   * @param hostnameResolver    rDNS lookup for remote clients ({@link InetAddress} to {@link String} resolver)
+   * @param hostnameResolver    rDNS lookup for remote clients ({@link InetAddress} to
+   *                            {@link String} resolver)
    * @param authenticator       {@link TokenAuthenticator} for incoming requests.
    * @param preprocessor        preprocessor.
    */
-  public RawLogsIngesterPortUnificationHandler(String handle,
-                                               @Nonnull LogsIngester ingester,
-                                               @Nonnull Function<InetAddress, String> hostnameResolver,
-                                               @Nonnull TokenAuthenticator authenticator,
-                                               @Nullable Supplier<ReportableEntityPreprocessor> preprocessor) {
+  public RawLogsIngesterPortUnificationHandler(
+      String handle, @Nonnull LogsIngester ingester,
+      @Nonnull Function<InetAddress, String> hostnameResolver,
+      @Nonnull TokenAuthenticator authenticator,
+      @Nullable Supplier<ReportableEntityPreprocessor> preprocessor) {
     super(authenticator, handle, true, true);
     this.logsIngester = ingester;
     this.hostnameResolver = hostnameResolver;
@@ -59,7 +67,8 @@ public class RawLogsIngesterPortUnificationHandler extends PortUnificationHandle
   @Override
   public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
     if (cause instanceof TooLongFrameException) {
-      logWarning("Received line is too long, consider increasing rawLogsMaxReceivedLength", cause, ctx);
+      logWarning("Received line is too long, consider increasing rawLogsMaxReceivedLength", cause,
+          ctx);
       return;
     }
     if (cause instanceof DecoderException) {
@@ -72,7 +81,9 @@ public class RawLogsIngesterPortUnificationHandler extends PortUnificationHandle
   @Override
   public void processLine(final ChannelHandlerContext ctx, String message) {
     if (message.isEmpty()) return;
-    ReportableEntityPreprocessor preprocessor = preprocessorSupplier == null ? null : preprocessorSupplier.get();
+    received.inc();
+    ReportableEntityPreprocessor preprocessor = preprocessorSupplier == null ?
+        null : preprocessorSupplier.get();
     String processedMessage = preprocessor == null ?
         message :
         preprocessor.forPointLine().transform(message);
