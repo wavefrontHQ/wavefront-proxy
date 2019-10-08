b96d026c6ea856c3bd3d4ebfb2a076d573729566
Anil Krishna Kodali <38334031+akodali18@users.noreply.github.com>
2019-10-07 17:46:10 -0700

    Add spanRenameTag preprocessor rule. (#458)

---
+++ proxy/src/main/java/com/wavefront/agent/preprocessor/SpanRenameAnnotationTransformer.java
@@ -0,0 +1,77 @@
+package com.wavefront.agent.preprocessor;
+
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+
+import com.yammer.metrics.core.Counter;
+
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import wavefront.report.Annotation;
+import wavefront.report.Span;
+
+/**
+ * Rename a given span tag's/annotation's (optional: if its value matches a regex pattern)
+ *
+ * If the tag matches multiple span annotation keys , all keys will be renamed.
+ *
+ * @author akodali@vmare.com
+ */
+public class SpanRenameAnnotationTransformer implements Function<Span, Span> {
+
+  private final String key;
+  private final String newKey;
+  @Nullable
+  private final Pattern compiledPattern;
+  private final boolean firstMatchOnly;
+  private final PreprocessorRuleMetrics ruleMetrics;
+
+  public SpanRenameAnnotationTransformer(final String key,
+                                         final String newKey,
+                                         @Nullable final String patternMatch,
+                                         final boolean firstMatchOnly,
+                                         final PreprocessorRuleMetrics ruleMetrics) {
+    this.key = Preconditions.checkNotNull(key, "[key] can't be null");
+    this.newKey = Preconditions.checkNotNull(newKey, "[newkey] can't be null");
+    Preconditions.checkArgument(!key.isEmpty(), "[key] can't be blank");
+    Preconditions.checkArgument(!newKey.isEmpty(), "[newkey] can't be blank");
+    this.compiledPattern = patternMatch != null ? Pattern.compile(patternMatch) : null;
+    Preconditions.checkNotNull(ruleMetrics, "PreprocessorRuleMetrics can't be null");
+    this.firstMatchOnly = firstMatchOnly;
+    this.ruleMetrics = ruleMetrics;
+  }
+
+  @Override
+  public Span apply(@Nonnull Span span) {
+    long startNanos = ruleMetrics.ruleStart();
+    if (span.getAnnotations() == null) {
+      ruleMetrics.ruleEnd(startNanos);
+      return span;
+    }
+
+    if (firstMatchOnly) {
+      Optional<Annotation> annotation = span.getAnnotations().stream().
+          filter(a -> a.getKey().equals(key) &&
+          (compiledPattern == null || compiledPattern.matcher(a.getValue()).matches())).findFirst();
+
+      if (annotation.isPresent()) {
+        annotation.get().setKey(newKey);
+      }
+      ruleMetrics.incrementRuleAppliedCounter();
+      ruleMetrics.ruleEnd(startNanos);
+      return span;
+    }
+
+    span.getAnnotations().stream().
+        filter(a -> a.getKey().equals(key) &&
+            (compiledPattern == null || compiledPattern.matcher(a.getValue()).matches())).
+        forEach(a -> a.setKey(newKey));
+    ruleMetrics.incrementRuleAppliedCounter();
+    ruleMetrics.ruleEnd(startNanos);
+    return span;
+  }
+}
