5bfc806deeb19c74330dcd3211c6180e2bf0cbd0
Yogesh Prasad <yogeshprasadkurmi@gmail.com>
2020-01-27 21:09:44 +0530

    This patch includes- (#482)
    Service should not stop after upgrading/downgrading
        Service should not be removed from chkconfig after upgrading/downgrading

--- pkg/after-remove.sh
+++ pkg/after-remove.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
-# Remove startup entries for wavefront-proxy.
-if [[ -f /etc/debian_version ]]; then
-	update-rc.d -f wavefront-proxy remove
-elif [[ -f /etc/redhat-release ]] || [[ -f /etc/system-release-cpe ]]; then
-	chkconfig --del wavefront-proxy
-elif [[ -f /etc/SUSE-brand ]]; then
-        systemctl disable wavefront-proxy
+# Remove startup entries for wavefront-proxy if operation is un-install.
+if [[ "$1" == "0" ]] || [[ "$1" == "remove" ]] || [[ "$1" == "purge" ]]; then
+	if [[ -f /etc/debian_version ]]; then
+		update-rc.d -f wavefront-proxy remove
+	elif [[ -f /etc/redhat-release ]] || [[ -f /etc/system-release-cpe ]]; then
+		chkconfig --del wavefront-proxy
+	elif [[ -f /etc/SUSE-brand ]]; then
+		systemctl disable wavefront-proxy
+	fi
 fi
 
 exit 0
