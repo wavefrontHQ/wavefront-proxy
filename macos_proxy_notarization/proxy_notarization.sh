f1b0d7485b1eebd5977bdb9a0c45f6e72effb745
“alchen1218” <alchen@vmware.com>
2020-05-11 02:30:16 -0700

    removing .jar creation in script

--- macos_proxy_notarization/proxy_notarization.sh
+++ macos_proxy_notarization/proxy_notarization.sh
@@ -107,8 +107,8 @@ rm -rf lib/*;
 ls
 mkdir lib/jdk;
 cp -r zulu11.39.15-ca-jdk11.0.7-macosx_x64/zulu-11.jdk/Contents/Home/* lib/jdk/;
-cp temp_new_WF_proxy/opt/wavefront/wavefront-proxy/bin/*.jar lib/proxy-uber.jar;
-cp temp_new_WF_proxy/etc/wavefront/wavefront-proxy/preprocessor_rules.yaml.default etc/preprocessor_rules.yaml;
+# cp temp_new_WF_proxy/opt/wavefront/wavefront-proxy/bin/*.jar lib/proxy-uber.jar;
+# cp temp_new_WF_proxy/etc/wavefront/wavefront-proxy/preprocessor_rules.yaml.default etc/preprocessor_rules.yaml;
 
 zip -r wavefront-proxy-$VERSION.zip bin/ etc/ lib/
 
