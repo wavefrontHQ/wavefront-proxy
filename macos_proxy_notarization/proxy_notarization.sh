d0545c0a38c6835f8f6dc058997c359cda437301
“alchen1218” <alchen@vmware.com>
2020-05-11 13:18:26 -0700

    changing wait time after upload

--- macos_proxy_notarization/proxy_notarization.sh
+++ macos_proxy_notarization/proxy_notarization.sh
@@ -129,7 +129,7 @@ echo "Grabbing Request UUID"
 requestuuid=${response#*= }
 echo $requestuuid
 
-sleep 20
+sleep 25
 
 echo "Executing this command to see the status of notarization"
 xcrun altool --notarization-info "$requestuuid" -u "$USERNAME" -p "$APP_SPECIFIC_PW"
