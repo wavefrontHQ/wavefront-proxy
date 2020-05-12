5487ec4bb6a6a879064c5f9e90cc6d37c40c7de8
“alchen1218” <alchen@vmware.com>
2020-05-11 18:41:16 -0700

    fixing jdk issue

--- macos_proxy_notarization/proxy_notarization.sh
+++ macos_proxy_notarization/proxy_notarization.sh
@@ -86,8 +86,11 @@ tar xvzf $tarfile
 echo "=============================================================="
 echo "=============================================================="
 
-# ls
-rm -rf lib/jdk;
+ls
+rm -rf lib/jdk
+mkdir lib/jdk
+lib ls
+lib/jdk ls
 cp -r zulu11.39.15-ca-jdk11.0.7-macosx_x64/zulu-11.jdk/Contents/Home/* lib/jdk/;
 # cp temp_new_WF_proxy/opt/wavefront/wavefront-proxy/bin/*.jar lib/proxy-uber.jar;
 # cp temp_new_WF_proxy/etc/wavefront/wavefront-proxy/preprocessor_rules.yaml.default etc/preprocessor_rules.yaml;
