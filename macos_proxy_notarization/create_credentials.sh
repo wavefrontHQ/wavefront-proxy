a8abb322c898b28d0584aa288d096133a1c34a2f
“alchen1218” <alchen@vmware.com>
2020-05-13 00:21:14 -0700

    adding aws-sunnylabs credentials

--- macos_proxy_notarization/create_credentials.sh
+++ macos_proxy_notarization/create_credentials.sh
@@ -6,4 +6,8 @@ cat > ~/.aws/credentials << EOL
 [default]
 aws_access_key_id=${AWS_ACCESS_KEY_ID}
 aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+
+[test]
+aws_access_key_id=${AWS_ACCESS_KEY_ID-test}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY-test}
 EOL
