fa918b4e359f4520f877c3432df156edb779e2ca
basilisk487 <vasily@wavefront.com>
2020-06-25 11:21:45 -0500

    More accurate task size calculation

--- proxy/src/main/java/com/wavefront/agent/queueing/TaskSizeEstimator.java
+++ proxy/src/main/java/com/wavefront/agent/queueing/TaskSizeEstimator.java
@@ -10,11 +10,9 @@ import com.yammer.metrics.core.Gauge;
 import com.yammer.metrics.core.Histogram;
 import com.yammer.metrics.core.Meter;
 import com.yammer.metrics.core.MetricsRegistry;
-import net.jpountz.lz4.LZ4BlockOutputStream;
 
 import javax.annotation.Nullable;
 import java.io.ByteArrayOutputStream;
-import java.io.ObjectOutputStream;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -39,6 +37,8 @@ public class TaskSizeEstimator {
    * A single threaded bounded work queue to update result posting sizes.
    */
   private final ExecutorService resultPostingSizerExecutorService;
+  @SuppressWarnings("rawtypes")
+  private final TaskConverter<DataSubmissionTask> taskConverter;
 
   /**
    * Only size postings once every 5 seconds.
@@ -57,6 +57,8 @@ public class TaskSizeEstimator {
     this.resultPostingSizerExecutorService = new ThreadPoolExecutor(1, 1,
         60L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(1),
         new NamedThreadFactory("result-posting-sizer-" + handle));
+    // for now, we can just use a generic task converter with default lz4 compression method
+    this.taskConverter = new RetryTaskConverter<>(handle, TaskConverter.CompressionType.LZ4);
     Metrics.newGauge(new TaggedMetricName("buffer", "fill-rate", "port", handle),
         new Gauge<Long>() {
           @Override
@@ -71,7 +73,7 @@ public class TaskSizeEstimator {
    * the rate limiter
    * @param task task to be sized.
    */
-  public void scheduleTaskForSizing(DataSubmissionTask<?> task) {
+  public <T extends DataSubmissionTask<T>> void scheduleTaskForSizing(T task) {
     resultPostingMeter.mark();
     try {
       //noinspection UnstableApiUsage
@@ -100,15 +102,11 @@ public class TaskSizeEstimator {
     resultPostingSizerExecutorService.shutdown();
   }
 
-  private Runnable getPostingSizerTask(final DataSubmissionTask<?> task) {
+  private <T extends DataSubmissionTask<T>> Runnable getPostingSizerTask(final T task) {
     return () -> {
       try {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        LZ4BlockOutputStream lz4OutputStream = new LZ4BlockOutputStream(outputStream);
-        ObjectOutputStream oos = new ObjectOutputStream(lz4OutputStream);
-        oos.writeObject(task);
-        oos.close();
-        lz4OutputStream.close();
+        taskConverter.serializeToStream(task, outputStream);
         resultPostingSizes.update(outputStream.size());
       } catch (Throwable t) {
         // ignored. this is a stats task.
