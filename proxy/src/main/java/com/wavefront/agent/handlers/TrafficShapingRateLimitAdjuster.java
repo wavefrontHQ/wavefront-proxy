19c6bb179bbc846e1ccee5298175e83d7ee204b8
Vasily V <16948475+basilisk487@users.noreply.github.com>
2020-08-06 15:25:41 -0500

    PUB-236: Use mean pps with headroom (#562)

--- proxy/src/main/java/com/wavefront/agent/handlers/TrafficShapingRateLimitAdjuster.java
+++ proxy/src/main/java/com/wavefront/agent/handlers/TrafficShapingRateLimitAdjuster.java
@@ -1,10 +1,10 @@
 package com.wavefront.agent.handlers;
 
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -12,11 +12,10 @@ import com.google.common.base.Preconditions;
 import com.google.common.util.concurrent.RecyclableRateLimiter;
 import com.wavefront.agent.data.EntityProperties;
 import com.wavefront.agent.data.EntityPropertiesFactory;
+import com.wavefront.common.EvictingRingBuffer;
 import com.wavefront.common.Managed;
+import com.wavefront.common.SynchronizedEvictingRingBuffer;
 import com.wavefront.data.ReportableEntityType;
-import com.yammer.metrics.core.Clock;
-import com.yammer.metrics.stats.ExponentiallyDecayingSample;
-import com.yammer.metrics.stats.Sample;
 
 /**
  * Experimental: use automatic traffic shaping (set rate limiter based on recently received
@@ -27,61 +26,43 @@ import com.yammer.metrics.stats.Sample;
 public class TrafficShapingRateLimitAdjuster extends TimerTask implements Managed {
   private static final Logger log =
       Logger.getLogger(TrafficShapingRateLimitAdjuster.class.getCanonicalName());
-  private static final int DEFAULT_SAMPLE_SIZE = 1028;
-  private static final double DEFAULT_ALPHA = 0.015;
   private static final int MIN_RATE_LIMIT = 10; // 10 pps
   private static final double TOLERANCE_PERCENT = 5.0;
 
   private final EntityPropertiesFactory entityProps;
-  private final double quantile;
   private final double headroom;
-  private final Map<ReportableEntityType, Sample> perEntityStats =
-      new EnumMap<>(ReportableEntityType.class);
-  private final Map<ReportableEntityType, AtomicLong> perEntitySamples =
+  private final Map<ReportableEntityType, EvictingRingBuffer<Long>> perEntityStats =
       new EnumMap<>(ReportableEntityType.class);
   private final Timer timer;
-  private final Clock clock;
-
-  /**
-   * @param entityProps       entity properties factory
-   * @param quantile          quantile of point rate to use
-   * @param headroom          headroom multiplier and minimum headroom requirement
-   */
-  public TrafficShapingRateLimitAdjuster(EntityPropertiesFactory entityProps, double quantile,
-                                         double headroom) {
-    this(entityProps, quantile, headroom, Clock.defaultClock());
-  }
+  private final int windowSeconds;
 
   /**
    * @param entityProps       entity properties factory (to control rate limiters)
-   * @param quantile          quantile of point rate to use
+   * @param windowSeconds     size of the moving time window to average point rate
    * @param headroom          headroom multiplier
-   * @param clock             clock for the exponentially decaying reservoir
    */
-  @VisibleForTesting
-  TrafficShapingRateLimitAdjuster(EntityPropertiesFactory entityProps, double quantile,
-                                  double headroom, Clock clock) {
+  public TrafficShapingRateLimitAdjuster(EntityPropertiesFactory entityProps,
+                                         int windowSeconds, double headroom) {
+    this.windowSeconds = windowSeconds;
     Preconditions.checkArgument(headroom >= 1.0, "headroom can't be less than 1!");
+    Preconditions.checkArgument(windowSeconds > 0, "windowSeconds needs to be > 0!");
     this.entityProps = entityProps;
-    this.quantile = quantile > 1 ? quantile / 100 : quantile;
     this.headroom = headroom;
-    this.clock = clock;
     this.timer = new Timer("traffic-shaping-adjuster-timer");
   }
 
   @Override
   public void run() {
     for (ReportableEntityType type : ReportableEntityType.values()) {
-      AtomicLong samples = perEntitySamples.computeIfAbsent(type, k -> new AtomicLong(0));
       EntityProperties props = entityProps.get(type);
       long rate = props.getTotalReceivedRate();
-      boolean hasBacklog = props.getTotalBacklogSize() > 0;
-      if (rate > 0 || samples.get() > 0) {
-        samples.incrementAndGet();
-        Sample stats = updateStats(type, rate);
-        if (samples.get() >= 300) { // need at least 5 minutes worth of stats to enable the limiter
+      EvictingRingBuffer<Long> stats = perEntityStats.computeIfAbsent(type, x ->
+          new SynchronizedEvictingRingBuffer<>(windowSeconds));
+      if (rate > 0 || stats.size() > 0) {
+        stats.add(rate);
+        if (stats.size() >= 60) { // need at least 1 minute worth of stats to enable the limiter
           RecyclableRateLimiter rateLimiter = props.getRateLimiter();
-          adjustRateLimiter(type, stats, rateLimiter, hasBacklog);
+          adjustRateLimiter(type, stats, rateLimiter);
         }
       }
     }
@@ -98,18 +79,11 @@ public class TrafficShapingRateLimitAdjuster extends TimerTask implements Manage
   }
 
   @VisibleForTesting
-  Sample updateStats(ReportableEntityType type, long rate) {
-    Sample sample = perEntityStats.computeIfAbsent(type, x ->
-        new ExponentiallyDecayingSample(DEFAULT_SAMPLE_SIZE, DEFAULT_ALPHA, clock));
-    sample.update(rate);
-    return sample;
-  }
-
-  @VisibleForTesting
-  void adjustRateLimiter(ReportableEntityType type, Sample sample,
-                         RecyclableRateLimiter rateLimiter, boolean backlog) {
-    double suggestedLimit = (MIN_RATE_LIMIT + sample.getSnapshot().getValue(quantile)) *
-        (backlog ? headroom : 1.0);
+  void adjustRateLimiter(ReportableEntityType type, EvictingRingBuffer<Long> sample,
+                         RecyclableRateLimiter rateLimiter) {
+    List<Long> samples = sample.toList();
+    double suggestedLimit = MIN_RATE_LIMIT + (samples.stream().mapToLong(i -> i).sum() /
+        (double) samples.size()) * headroom;
     double currentRate = rateLimiter.getRate();
     if (Math.abs(currentRate - suggestedLimit) > currentRate * TOLERANCE_PERCENT / 100) {
       log.fine("Setting rate limit for " + type.toString() + " to " + suggestedLimit);
