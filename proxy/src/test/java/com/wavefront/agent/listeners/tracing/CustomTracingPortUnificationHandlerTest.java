7cc04f02bd23fdc7364953f3c48e1a5c80db71ae
Peter Stone <stonepe@vmware.com>
2021-02-23 11:50:25 -0700

    PUB-287 - report span duration in microseconds, not milliseconds

---
+++ proxy/src/test/java/com/wavefront/agent/listeners/tracing/CustomTracingPortUnificationHandlerTest.java
@@ -0,0 +1,67 @@
+package com.wavefront.agent.listeners.tracing;
+
+import com.google.common.collect.ImmutableList;
+import com.wavefront.agent.handlers.MockReportableEntityHandlerFactory;
+import com.wavefront.agent.handlers.ReportableEntityHandler;
+import com.wavefront.internal.reporter.WavefrontInternalReporter;
+import com.wavefront.internal_reporter_java.io.dropwizard.metrics5.DeltaCounter;
+import com.wavefront.internal_reporter_java.io.dropwizard.metrics5.WavefrontHistogram;
+import org.easymock.Capture;
+import org.easymock.EasyMock;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+import wavefront.report.Annotation;
+import wavefront.report.Span;
+
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.assertEquals;
+
+public class CustomTracingPortUnificationHandlerTest {
+    @Test
+    public void reportsCorrectDuration() {
+        WavefrontInternalReporter reporter = EasyMock.niceMock(WavefrontInternalReporter.class);
+        expect(reporter.newDeltaCounter(anyObject())).andReturn(new DeltaCounter()).anyTimes();
+        WavefrontHistogram histogram = EasyMock.niceMock(WavefrontHistogram.class);
+        expect(reporter.newWavefrontHistogram(anyObject(), anyObject())).andReturn(histogram);
+        Capture<Long> duration = newCapture();
+        histogram.update(captureLong(duration));
+        expectLastCall();
+        ReportableEntityHandler<Span, String> handler = MockReportableEntityHandlerFactory.getMockTraceHandler();
+        CustomTracingPortUnificationHandler subject = new CustomTracingPortUnificationHandler(
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                handler,
+                null,
+                null,
+                null,
+                null,
+                null,
+                reporter,
+                null,
+                null,
+                null);
+        replay(reporter, histogram);
+
+        Span span = getSpan();
+        span.setDuration(1000); // milliseconds
+        subject.report(span);
+        verify(reporter, histogram);
+        long value = duration.getValue();
+        assertEquals(1000000, value); // microseconds
+    }
+
+    @NotNull
+    private Span getSpan() {
+        Span span = new Span();
+        span.setAnnotations(ImmutableList.of(
+                new Annotation("application", "application"),
+                new Annotation("service", "service")
+        ));
+        return span;
+    }
+
+}
