07292e51e1e26e8e2d4329f465b5915cfe866d47
formanojhr <formanojr@gmail.com>
2021-04-08 18:06:40 -0700

    doing a null check for weight before initializing weight

--- proxy/src/main/java/com/wavefront/agent/queueing/FileBasedTaskQueue.java
+++ proxy/src/main/java/com/wavefront/agent/queueing/FileBasedTaskQueue.java
@@ -83,9 +83,13 @@ public class FileBasedTaskQueue<T extends DataSubmissionTask<T>> implements Task
       this.head = taskConverter.fromBytes(task);
     }
     queueFile.remove();
-    int weight = this.head.weight();
-    currentWeight.getAndUpdate(x -> x > weight ? x - weight : 0);
-    this.head = null;
+    int weight = 0;
+    if(this.head != null) {
+      weight = this.head.weight();
+      int finalWeight = weight;
+      currentWeight.getAndUpdate(x -> x > finalWeight ? x - finalWeight : 0);
+      this.head = null;
+    }
   }
 
   @Override
