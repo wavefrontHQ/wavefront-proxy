d1bf2d8c0c66e8bd64bb2e235adf16f73255572b
Manoj Ramakrishnan <formanojr@gmail.com>
2021-11-05 16:08:32 -0700

    change comments

--- proxy/src/main/java/com/wavefront/agent/logforwarder/ingestion/processors/PostToRDCLogIqProcessor.java
+++ proxy/src/main/java/com/wavefront/agent/logforwarder/ingestion/processors/PostToRDCLogIqProcessor.java
@@ -11,7 +11,7 @@ import com.wavefront.agent.logforwarder.ingestion.util.LogForwarderUtils;
 import com.wavefront.agent.logforwarder.ingestion.util.UriUtils;
 
 /**
- * Processor
+ * Processor initializer  Logs SaaS backend with ingestion key
  * @author Manoj Ramakrishnan (rmanoj@vmware.com).
  * @since 9/2/21 4:14 PM
  */
@@ -30,11 +30,9 @@ public class PostToRDCLogIqProcessor extends PostToLogIqProcessor implements Pro
       chainName = jsonObject.get(LogForwarderConstants.CHAIN_NAME).toString();
     }
 
-//    url = LogForwarderConfigProperties.logForwarderArgs.lemansServerUrl;//TODO Move this to
     // proxyConfig
     url = jsonObject.get(LogForwarderConstants.INGESTION_GATEWAY_URL).toString();
 
-//    accessKey = LogForwarderUtils.getLemansClientAccessKey();//TODO move this to ProxyConfig
     accessKey =  jsonObject.get(LogForwarderConstants.INGESTION_GATEWAY_ACCESS_TOKEN).toString();
     tenantIdentifier = LogForwarderConstants.RDC_TENANT_IDENTIFIER;
     bufferDiskLocation = jsonObject.get(LogForwarderConstants.INGESTION_DISK_QUEUE_LOCATION).toString();
@@ -44,7 +42,7 @@ public class PostToRDCLogIqProcessor extends PostToLogIqProcessor implements Pro
       throw e;
     }
     streamUri = UriUtils.extendUri(UriUtils.buildUri(url), "le-mans/v1/streams/ingestion-pipeline" +
-        "-stream");//TODO move this to some other generic URI helper not xenon
+        "-stream");
     HttpClientUtils
         .createAsyncHttpClient(accessKey, (int) TimeUnit.SECONDS.toMillis(httpTimeOutSecs), Boolean.TRUE);
     httpAsyncClient = HttpClientUtils.getHttpClient(accessKey);
