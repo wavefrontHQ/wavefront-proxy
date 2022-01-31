2fc3cbc1743f087d64de44b6cc280d8c7520ddee
German Laullon <glaullon@vmware.com>
2022-01-31 23:52:30 +0100

    New build system (#693)
    * [MONIT-25601] Enable multiplatform support for arm64 for AWS deployments
    * [MONIT-26264] Proxy docker build system
    * [MONIT-25838] Consolidate Proxy pom.xml
    * [MONIT-25656] Review Linux packaging
    * [MONIT-25955] Add packages for Oracle Linux 8 in packagecloud for proxy

---
+++ Makefile
@@ -0,0 +1,73 @@
+TS = $(shell date +%Y%m%d-%H%M%S)
+
+VERSION = $(shell mvn -f proxy -q -Dexec.executable=echo -Dexec.args='$${project.version}' --non-recursive exec:exec)
+REVISION ?= ${TS}
+FULLVERSION = ${VERSION}_${REVISION}
+USER ?= $(LOGNAME)
+REPO ?= proxy-dev
+PACKAGECLOUD_USER ?= wavefront
+PACKAGECLOUD_REPO ?= proxy-next
+
+DOCKER_TAG = $(USER)/$(REPO):${FULLVERSION}
+
+out = $(shell pwd)/out
+$(shell mkdir -p $(out))
+
+.info:
+	@echo "\n----------\nBuilding Proxy ${FULLVERSION}\nDocker tag: ${DOCKER_TAG}\n----------\n"
+
+jenkins: .info build-jar build-linux push-linux docker-multi-arch clean
+
+#####
+# Build Proxy jar file
+#####
+build-jar: .info
+	mvn -f proxy --batch-mode package 
+	cp proxy/target/proxy-${VERSION}-uber.jar ${out}
+
+#####
+# Build single docker image
+#####
+docker: .info .cp-docker
+	docker build -t $(DOCKER_TAG) docker/
+
+
+#####
+# Build multi arch (amd64 & arm64) docker images
+#####
+docker-multi-arch: .info .cp-docker
+	docker buildx create --use
+	docker buildx build --platform linux/amd64,linux/arm64 -t $(DOCKER_TAG) --push docker/
+
+
+#####
+# Build rep & deb packages
+#####
+build-linux: .info .prepare-builder .cp-linux
+	docker run -v $(shell pwd)/:/proxy proxy-linux-builder /proxy/pkg/build.sh ${VERSION} ${REVISION}
+	
+#####
+# Push rep & deb packages
+#####
+push-linux: .info .prepare-builder
+	docker run -v $(shell pwd)/:/proxy proxy-linux-builder /proxy/pkg/upload_to_packagecloud.sh ${PACKAGECLOUD_USER}/${PACKAGECLOUD_REPO} /proxy/pkg/package_cloud.conf /proxy/out
+
+.prepare-builder:
+	docker build -t proxy-linux-builder pkg/
+
+.cp-docker:
+	cp ${out}/proxy-${VERSION}-uber.jar docker/wavefront-proxy.jar
+	${MAKE} .set_package JAR=docker/wavefront-proxy.jar PKG=docker
+
+.cp-linux:
+	cp ${out}/proxy-${VERSION}-uber.jar pkg/wavefront-proxy.jar
+	${MAKE} .set_package JAR=pkg/wavefront-proxy.jar PKG=linux_rpm_deb
+
+clean:
+	docker buildx prune -a -f	
+
+.set_package:
+	jar -xvf ${JAR} build.properties
+	sed 's/\(build.package=\).*/\1${PKG}/' build.properties > build.tmp && mv build.tmp build.properties
+	jar -uvf ${JAR} build.properties
+	rm build.properties
--- pkg/build.sh
+++ pkg/build.sh
@@ -1,71 +1,53 @@
 #!/bin/bash -ex
 
-function die {
-	echo $@
-	exit 1
-}
+VERSION=$1
+ITERATION=$2
+cd $(dirname $0)
 
-cd `dirname $0`
+rm -rf build out
 
-if [[ $# -eq 3 ]]; then
-	FPM_TARGET=$1
-	VERSION=$2
-	ITERATION=$3
-elif [[ $# -eq 1 ]]; then
-	FPM_TARGET=$1
-	# Automatically get next version/iteration based on packagecloud and current repo.
-	MOST_RECENT_VERSION=$(git tag | grep wavefront- | sed -e 's/wavefront-//' | sort -V | tail -1 | tr -d '[[:space:]]')
-	YUM_VERSION=$(yum info wavefront-proxy | grep Version | cut -d: -f2 | tr -d '[[:space:]]')
-	YUM_ITERATION=$(yum info wavefront-proxy | grep Release | cut -d: -f2 | tr -d '[[:space:]]')
-	VERSION=$MOST_RECENT_VERSION
-	if [[ "$MOST_RECENT_VERSION" == "$YUM_VERSION" ]]; then
-		let "ITERATION=YUM_ITERATION+1"
-	else
-		ITERATION=1
-	fi
-else
-	die "Usage: $0 <fpm_target> [<fpm_version> <fpm_iteration>]"
-fi
+mkdir build
+cp -r opt build/opt
+cp -r etc build/etc
+cp -r usr build/usr
 
-# Get the license file in to a good place, depending on the distro.
-LICENSES_SYM=open_source_licenses.txt
-LICENSES_FILE=$(readlink -f $LICENSES_SYM)
+mkdir -p build/opt/wavefront/wavefront-proxy/bin
+mkdir -p build/usr/share/doc/wavefront-proxy/
+mkdir -p build/opt/wavefront/wavefront-proxy
 
-if [[ $FPM_TARGET == "deb" ]]; then
-	EXTRA_DIRS="usr"
-	cp $LICENSES_FILE build/usr/share/doc/wavefront-proxy/open_source_licenses.txt
-else
-	cp $LICENSES_FILE build/opt/wavefront/wavefront-proxy
-	EXTRA_DIRS=""
-fi
+cp ../open_source_licenses.txt build/usr/share/doc/wavefront-proxy/
+cp ../open_source_licenses.txt build/opt/wavefront/wavefront-proxy
+cp /opt/commons-daemon/src/native/unix/jsvc build/opt/wavefront/wavefront-proxy/bin
+cp wavefront-proxy.jar build/opt/wavefront/wavefront-proxy/bin
 
-fpm \
-	--after-install after-install.sh \
-	--before-remove before-remove.sh \
-	--after-remove after-remove.sh \
-	--architecture amd64 \
-	--deb-no-default-config-files \
-	--deb-priority optional \
-	--depends curl \
-	--description "Proxy for sending data to Wavefront." \
-	--exclude "*/.git" \
-	--iteration $ITERATION \
-	--license "Apache 2.0" \
-	--log info \
-	--maintainer "Wavefront" \
-	--name wavefront-proxy \
-	--rpm-os linux \
-	--url https://www.wavefront.com \
-	--vendor Wavefront \
-	--version $VERSION \
-	-C build \
-	-s dir \
-	-t $FPM_TARGET \
-	opt etc $EXTRA_DIRS
+for target in deb rpm
+do
+	fpm \
+		--after-install after-install.sh \
+		--before-remove before-remove.sh \
+		--after-remove after-remove.sh \
+		--architecture amd64 \
+		--deb-no-default-config-files \
+		--deb-priority optional \
+		--depends curl,tar \
+		--description "Proxy for sending data to Wavefront." \
+		--exclude "*/.git" \
+		--iteration $ITERATION \
+		--license "Apache 2.0" \
+		--log info \
+		--maintainer "Wavefront" \
+		--name wavefront-proxy \
+		--rpm-os linux \
+		--url https://www.wavefront.com \
+		--vendor Wavefront \
+		--version $VERSION \
+		-C build \
+		-s dir \
+		-t ${target} \
+		opt etc usr
 
-if [[ $FPM_TARGET == "rpm" ]]; then
-	rpm --delsign *.rpm
-fi
+	[[ -d out ]] || mkdir out
+	mv *.${target} ../out
+done 
 
-[[ -d out ]] || mkdir out
-mv *.$FPM_TARGET out
+rpm --delsign ../out/*.rpm
