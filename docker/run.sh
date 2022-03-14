39a63bc7423c6bb268254a23a25bbc1064cd6362
German Laullon <glaullon@vmware.com>
2022-03-14 14:26:37 +0100

    some changes

--- docker/run.sh
+++ docker/run.sh
@@ -26,13 +26,6 @@ java_heap_usage=${JAVA_HEAP_USAGE:-4G}
 jvm_initial_ram_percentage=${JVM_INITIAL_RAM_PERCENTAGE:-50.0}
 jvm_max_ram_percentage=${JVM_MAX_RAM_PERCENTAGE:-85.0}
 
-# Use cgroup opts - Note that -XX:UseContainerSupport=true since Java 8u191.
-# https://bugs.openjdk.java.net/browse/JDK-8146115
-jvm_container_opts="-XX:InitialRAMPercentage=$jvm_initial_ram_percentage  -XX:MaxRAMPercentage=$jvm_max_ram_percentage"
-if [ "${JVM_USE_CONTAINER_OPTS}" = false ] ; then
-    jvm_container_opts="-Xmx$java_heap_usage -Xms$java_heap_usage"
-fi
-
 ###################
 # import CA certs #
 ###################
@@ -56,7 +49,7 @@ fi
 # run proxy #
 #############
 java \
-    $jvm_container_opts $JAVA_ARGS \
+  $JAVA_ARGS \
 	-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager \
 	-Dlog4j.configurationFile=/etc/wavefront/wavefront-proxy/log4j2.xml \
 	-jar /opt/wavefront/wavefront-proxy/wavefront-proxy.jar \
