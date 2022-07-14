7c94529e389a49a5df92d8aa4fb6109a671505f0
Joanna Ko <69543190+joannak-vmware@users.noreply.github.com>
2022-07-14 11:18:55 -0700

    Fix SourceTagSubmissionTask Failing Tests (#770)

--- proxy/src/test/java/com/wavefront/agent/data/SourceTagSubmissionTaskTest.java
+++ proxy/src/test/java/com/wavefront/agent/data/SourceTagSubmissionTaskTest.java
@@ -1,8 +1,8 @@
 package com.wavefront.agent.data;
 
-import static com.wavefront.agent.ProxyContext.queuesManager;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.replay;
 import static org.junit.Assert.assertEquals;
 
 import com.google.common.collect.ImmutableList;
@@ -27,6 +27,9 @@ public class SourceTagSubmissionTaskTest {
   private final EntityProperties props = new DefaultEntityPropertiesForTesting();
   private SourceTagAPI sourceTagAPI = EasyMock.createMock(SourceTagAPI.class);
 
+  private QueueInfo queue = EasyMock.createMock(QueueInfo.class);
+  private Response mockResponse = EasyMock.createMock(Response.class);
+
   @Test
   public void test200() {
     ReportSourceTag sourceDescDelete =
@@ -41,7 +44,9 @@ public class SourceTagSubmissionTaskTest {
     ReportSourceTag sourceTagAdd =
         new ReportSourceTag(
             SourceOperationType.SOURCE_TAG, SourceTagAction.ADD, "src", ImmutableList.of("tag"));
-    QueueInfo queue = queuesManager.initQueue(ReportableEntityType.SOURCE_TAG);
+    expect(queue.getName()).andReturn("").anyTimes();
+    expect(queue.getEntityType()).andReturn(ReportableEntityType.SOURCE_TAG).anyTimes();
+    replay(queue);
     SenderStats stats = SenderStats.create(queue, new ScheduledThreadPoolExecutor(0));
     SourceTagSubmissionTask task =
         new SourceTagSubmissionTask(
@@ -67,29 +72,29 @@ public class SourceTagSubmissionTaskTest {
             new SourceTag(sourceTagAdd),
             System::currentTimeMillis,
             stats);
+    expect(mockResponse.getStatus()).andReturn(200).times(3);
     expect(sourceTagAPI.removeDescription("dummy")).andReturn(Response.status(200).build()).once();
     expect(sourceTagAPI.removeTag("src", "tag")).andReturn(Response.status(200).build()).once();
     expect(sourceTagAPI.appendTag("src", "tag")).andReturn(Response.status(200).build()).once();
-    assertEquals(TaskResult.DELIVERED, task.execute());
-    assertEquals(TaskResult.DELIVERED, task2.execute());
-    assertEquals(TaskResult.DELIVERED, task3.execute());
+    replay(sourceTagAPI, mockResponse);
+    // Note: Changed TaskResult.DELIVERED to 0 as AbstractDataSubmissionTask execute() has
+    // changed return type from TaskResult to int
+    assertEquals(0, task.execute());
+    assertEquals(0, task2.execute());
+    assertEquals(0, task3.execute());
   }
 
-  @Test
-  public void test404() throws Exception {
+  @Test(expected = RuntimeException.class)
+  public void test404_RemoveDescription() {
     ReportSourceTag sourceDescDelete =
         new ReportSourceTag(
             SourceOperationType.SOURCE_DESCRIPTION,
             SourceTagAction.DELETE,
             "dummy",
             ImmutableList.of());
-    ReportSourceTag sourceTagDelete =
-        new ReportSourceTag(
-            SourceOperationType.SOURCE_TAG, SourceTagAction.DELETE, "src", ImmutableList.of("tag"));
-    ReportSourceTag sourceTagAdd =
-        new ReportSourceTag(
-            SourceOperationType.SOURCE_TAG, SourceTagAction.ADD, "src", ImmutableList.of("tag"));
-    QueueInfo queue = queuesManager.initQueue(ReportableEntityType.SOURCE_TAG);
+    expect(queue.getName()).andReturn("").anyTimes();
+    expect(queue.getEntityType()).andReturn(ReportableEntityType.SOURCE_TAG).anyTimes();
+    replay(queue);
     SenderStats stats = SenderStats.create(queue, new ScheduledThreadPoolExecutor(0));
     SourceTagSubmissionTask task =
         new SourceTagSubmissionTask(
@@ -99,6 +104,24 @@ public class SourceTagSubmissionTaskTest {
             new SourceTag(sourceDescDelete),
             System::currentTimeMillis,
             stats);
+    expect(mockResponse.getStatus()).andReturn(404).once();
+    expect(sourceTagAPI.removeDescription("dummy")).andReturn(Response.status(404).build()).once();
+    expectLastCall();
+    replay(sourceTagAPI, mockResponse);
+
+    // Currently, status 404 returns RuntimeException("Unhandled DataSubmissionException", ex)
+    task.execute();
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void test404_RemoveTag() {
+    ReportSourceTag sourceTagDelete =
+        new ReportSourceTag(
+            SourceOperationType.SOURCE_TAG, SourceTagAction.DELETE, "src", ImmutableList.of("tag"));
+    expect(queue.getName()).andReturn("").anyTimes();
+    expect(queue.getEntityType()).andReturn(ReportableEntityType.SOURCE_TAG).anyTimes();
+    replay(queue);
+    SenderStats stats = SenderStats.create(queue, new ScheduledThreadPoolExecutor(0));
     SourceTagSubmissionTask task2 =
         new SourceTagSubmissionTask(
             sourceTagAPI,
@@ -107,6 +130,24 @@ public class SourceTagSubmissionTaskTest {
             new SourceTag(sourceTagDelete),
             System::currentTimeMillis,
             stats);
+    expect(mockResponse.getStatus()).andReturn(404).once();
+    expect(sourceTagAPI.removeTag("src", "tag")).andReturn(Response.status(404).build()).once();
+    expectLastCall();
+    replay(sourceTagAPI, mockResponse);
+
+    // Currently, status 404 returns RuntimeException("Unhandled DataSubmissionException", ex)
+    task2.execute();
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void test404_AddTag() {
+    ReportSourceTag sourceTagAdd =
+        new ReportSourceTag(
+            SourceOperationType.SOURCE_TAG, SourceTagAction.ADD, "src", ImmutableList.of("tag"));
+    expect(queue.getName()).andReturn("").anyTimes();
+    expect(queue.getEntityType()).andReturn(ReportableEntityType.SOURCE_TAG).anyTimes();
+    replay(queue);
+    SenderStats stats = SenderStats.create(queue, new ScheduledThreadPoolExecutor(0));
     SourceTagSubmissionTask task3 =
         new SourceTagSubmissionTask(
             sourceTagAPI,
@@ -115,18 +156,16 @@ public class SourceTagSubmissionTaskTest {
             new SourceTag(sourceTagAdd),
             System::currentTimeMillis,
             stats);
-    expect(sourceTagAPI.removeDescription("dummy")).andReturn(Response.status(404).build()).once();
-    expect(sourceTagAPI.removeTag("src", "tag")).andReturn(Response.status(404).build()).once();
+    expect(mockResponse.getStatus()).andReturn(404).once();
     expect(sourceTagAPI.appendTag("src", "tag")).andReturn(Response.status(404).build()).once();
     expectLastCall();
+    replay(sourceTagAPI, mockResponse);
 
-    assertEquals(TaskResult.DELIVERED, task.execute());
-    assertEquals(TaskResult.DELIVERED, task2.execute());
-    assertEquals(TaskResult.PERSISTED, task3.execute());
+    task3.execute();
   }
 
   @Test
-  public void test500() throws Exception {
+  public void test500() {
     ReportSourceTag sourceDescDelete =
         new ReportSourceTag(
             SourceOperationType.SOURCE_DESCRIPTION,
@@ -139,7 +178,9 @@ public class SourceTagSubmissionTaskTest {
     ReportSourceTag sourceTagAdd =
         new ReportSourceTag(
             SourceOperationType.SOURCE_TAG, SourceTagAction.ADD, "src", ImmutableList.of("tag"));
-    QueueInfo queue = queuesManager.initQueue(ReportableEntityType.SOURCE_TAG);
+    expect(queue.getName()).andReturn("").anyTimes();
+    expect(queue.getEntityType()).andReturn(ReportableEntityType.SOURCE_TAG).anyTimes();
+    replay(queue);
     SenderStats stats = SenderStats.create(queue, new ScheduledThreadPoolExecutor(0));
     SourceTagSubmissionTask task =
         new SourceTagSubmissionTask(
@@ -165,12 +206,16 @@ public class SourceTagSubmissionTaskTest {
             new SourceTag(sourceTagAdd),
             System::currentTimeMillis,
             stats);
+    expect(mockResponse.getStatus()).andReturn(500).once();
     expect(sourceTagAPI.removeDescription("dummy")).andReturn(Response.status(500).build()).once();
     expect(sourceTagAPI.removeTag("src", "tag")).andReturn(Response.status(500).build()).once();
     expect(sourceTagAPI.appendTag("src", "tag")).andReturn(Response.status(500).build()).once();
     expectLastCall();
-    assertEquals(TaskResult.PERSISTED, task.execute());
-    assertEquals(TaskResult.PERSISTED, task2.execute());
-    assertEquals(TaskResult.PERSISTED, task3.execute());
+    replay(sourceTagAPI, mockResponse);
+
+    // Right now we are not throwing IgnoreStatusCodeException for 500
+    assertEquals(500, task.execute());
+    assertEquals(500, task2.execute());
+    assertEquals(500, task3.execute());
   }
 }
