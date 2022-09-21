f21e3407adcd2bc5c393885cc0f81ad276cd7e82
German Laullon <glaullon@vmware.com>
2022-09-21 12:03:56 +0200

    better buffer test

--- tests/disk-buffer/helm/scripts/test_metrics.sh
+++ tests/disk-buffer/helm/scripts/test_metrics.sh
@@ -1,27 +1,35 @@
 #!/bin/bash -xe
-echo "Waiting proxy to open on 2878..."
 
-while ! bash -c "echo > /dev/tcp/localhost/2878"; do   
-  sleep 1
-done
-
-echo "done"
-
-ID=${PROXY_ID:=$(cat "/tmp/id")}
+wait_proxy_up(){
+    echo "Waiting proxy to open on 2878..."
+    while ! bash -c "echo > /dev/tcp/localhost/2878"; do   
+    sleep 1
+    done
+    echo "done"
+}
 
 truncate_buffer(){
     curl \
-        -X 'PUT' \
+        --silent -X 'PUT' \
         -H 'Content-Type: application/json' \
         -H "Authorization: Bearer ${WAVEFRONT_TOKEN}" \
         "${WAVEFRONT_URL}v2/proxy/${ID}" \
         -d '{"shutdown":false ,"truncate":true}'
 }
 
+shutdown_proxy(){
+    curl \
+        --silent -X 'PUT' \
+        -H 'Content-Type: application/json' \
+        -H "Authorization: Bearer ${WAVEFRONT_TOKEN}" \
+        "${WAVEFRONT_URL}v2/proxy/${ID}" \
+        -d '{"shutdown":true ,"truncate":false}'
+}
+
 get_buffer_points(){
     test=$(curl \
             --silent -X 'GET' \
-            "${WAVEFRONT_URL}v2/chart/raw?source=disk-buffer-test-proxy&metric=~proxy.buffer.disk.points.points" \
+            "${WAVEFRONT_URL}v2/chart/raw?source=disk-buffer-test-proxy&metric=~proxy.buffer.${1}.points.points" \
             -H 'accept: application/json' \
             -H "Authorization: Bearer ${WAVEFRONT_TOKEN}")
     points=$(echo $test | jq 'map(.points) | flatten | sort_by(.timestamp)[-1].value')
@@ -32,22 +40,38 @@ wait_buffer_have_points(){
     while true
     do
         sleep 15
-        v=$(get_buffer_points)
+        v=$(get_buffer_points $1)
         echo "${v}"
-        if [ "${v}" -eq "${1}" ]
+        if [ "${v}" -eq "${2}" ]
         then
             return
         fi
     done
 }
 
-METRICNAME_A="test.gh.buffer-disk.${RANDOM}${RANDOM}"
-for i in {0..99}
-do
-    curl http://localhost:2878 -X POST -d "${METRICNAME_A} ${RANDOM} source=github_proxy_action"
-done
+send_metrics(){
+    METRICNAME_A="test.gh.buffer-disk.${RANDOM}${RANDOM}"
+    for i in {0..99}
+    do
+        curl http://localhost:2878 -X POST -d "${METRICNAME_A} ${RANDOM} source=github_proxy_action"
+    done
+}
 
+/bin/bash /opt/wavefront/wavefront-proxy/run.sh & 
+wait_proxy_up
+ID=${PROXY_ID:=$(cat "/tmp/id")}
 
-wait_buffer_have_points 100
+wait_buffer_have_points memory 0
+send_metrics
+wait_buffer_have_points memory 100
+shutdown_proxy
+
+sleep 120
+
+/bin/bash /opt/wavefront/wavefront-proxy/run.sh & 
+wait_buffer_have_points memory 0
+wait_buffer_have_points disk 100
 truncate_buffer
-wait_buffer_have_points 0
+wait_buffer_have_points disk 0
+
+shutdown_proxy
