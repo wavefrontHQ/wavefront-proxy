eb64acd58a93ee7f857dec9c75fd5547aa13fa7f
Sean Chen <seanch@vmware.com>
2022-09-28 16:54:59 -0400

    MONIT-30933: fix data submission serialization

--- proxy/src/main/java/com/wavefront/agent/data/LogDataSubmissionTask.java
+++ proxy/src/main/java/com/wavefront/agent/data/LogDataSubmissionTask.java
@@ -29,6 +29,7 @@ public class LogDataSubmissionTask extends AbstractDataSubmissionTask<LogDataSub
   private transient UUID proxyId;
 
   @JsonProperty private List<Log> logs;
+  @JsonProperty
   private int weight;
 
   @SuppressWarnings("unused")
