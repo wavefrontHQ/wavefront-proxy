5701d1fc470b11c4319aafdcebd7992dbd374215
Sean Chen <seanch@vmware.com>
2022-10-03 19:13:31 -0400

    fmt

--- proxy/src/main/java/com/wavefront/agent/data/LogDataSubmissionTask.java
+++ proxy/src/main/java/com/wavefront/agent/data/LogDataSubmissionTask.java
@@ -29,8 +29,7 @@ public class LogDataSubmissionTask extends AbstractDataSubmissionTask<LogDataSub
   private transient UUID proxyId;
 
   @JsonProperty private List<Log> logs;
-  @JsonProperty
-  private int weight;
+  @JsonProperty private int weight;
 
   @SuppressWarnings("unused")
   LogDataSubmissionTask() {}
