881abb63c249083ce23a138318a68d1f430ff125
German Laullon <glaullon@vmware.com>
2022-10-25 12:22:00 +0200

    Proto file on the Proxy maven project

--- tests/util/gRPCclient/src/main/java/com/wavefront/grpc/example/SimpleTest.java
+++ tests/util/gRPCclient/src/main/java/com/wavefront/grpc/example/SimpleTest.java
@@ -1,9 +1,6 @@
 package com.wavefront.grpc.example;
 
-import com.wavefront.grpc.Batch;
-import com.wavefront.grpc.Point;
-import com.wavefront.grpc.Response;
-import com.wavefront.grpc.WFProxyGrpc;
+import com.wavefront.grpc.*;
 import io.grpc.Channel;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -23,17 +20,14 @@ public class SimpleTest {
   }
 
   public static void main(String[] args) throws Exception {
-    String metricName = "test.metric.grpc";
     String wfProxy = "localhost:50051";
     if (args.length > 0) {
       if ("--help".equals(args[0])) {
-        System.err.println("Usage: [name [wfProxy]]");
+        System.err.println("Usage: [wfProxy]");
         System.err.println();
-        System.err.println("  name    The name you wish to be greeted by. Defaults to " + metricName);
         System.err.println("  wfProxy  The server to connect to. Defaults to " + wfProxy);
         System.exit(1);
       }
-      metricName = args[0];
     }
     if (args.length > 1) {
       wfProxy = args[1];
@@ -42,32 +36,66 @@ public class SimpleTest {
     ManagedChannel channel = ManagedChannelBuilder.forTarget(wfProxy).usePlaintext().build();
     try {
       SimpleTest client = new SimpleTest(channel);
-      client.sendMetric(metricName);
+      client.sendMetrics();
     } finally {
       channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
     }
   }
 
-  public void sendMetric(String name) {
-    logger.info("Will try to send '" + name + "' metrics to WF ...");
+  public void sendMetrics() {
     Point point = Point.newBuilder()
-            .setName(name)
+            .setName("test.metric.a")
             .setTimestamp(System.currentTimeMillis())
             .setSource("sampleApp")
-            .setValue((int) (Math.random() * 1000))
+            .setValue(Math.random() * 1000d)
             .putTags("tag1", "tagv1")
             .putTags("tag2", "tagv2")
             .putTags("tag3", "tagv3")
             .build();
 
     Point point2 = Point.newBuilder()
-            .setName(name).setValue((int) (Math.random() * 1000))
+            .setName("test.metric.b")
+            .setValue(Math.random() * 1000d)
             .build();
 
+    Histogram.Builder histogramBuilder = Histogram.newBuilder()
+            .setName("test.histogram.a");
+    for (int i = 0; i < 10; i++) {
+      histogramBuilder.addBinsBuilder().setCount(i).setMean(Math.random() * 1000d).build();
+    }
+
+    Batch request = Batch.newBuilder()
+            .addPoints(point)
+            .addPoints(point2)
+            .addHistograms(histogramBuilder.build())
+            .build();
     Response response;
-    Batch request = Batch.newBuilder().addPoints(point).addPoints(point2).build();
     try {
       response = blockingStub.reportPoints(request);
+    } catch (StatusRuntimeException e) {
+      logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+      return;
+    }
+    logger.info("result: " + response.getRes());
+
+
+    Batch.Builder requestWithDefaultsBuilder = Batch.newBuilder()
+            .setDefaultSource("default_source")
+            .setDefaultTimestamp(System.currentTimeMillis())
+            .putCommonTags("app", this.getClass().getSimpleName());
+    requestWithDefaultsBuilder.addPointsBuilder()
+            .setName("test.metric.a")
+            .setValue(Math.random() * 1000d)
+            .build();
+    requestWithDefaultsBuilder.addPointsBuilder()
+            .setName("test.metric.b")
+            .setValue(Math.random() * 1000d)
+            .setType(POINT_TYPE.DELTA)
+            .putTags("service", "main")
+            .build();
+
+    try {
+      response = blockingStub.reportPoints(requestWithDefaultsBuilder.build());
     } catch (StatusRuntimeException e) {
       logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
       return;
