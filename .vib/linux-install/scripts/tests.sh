29a1228ad4b3a775fd58ebb3901b36f09c76a0c5
German Laullon <glaullon@vmware.com>
2022-10-31 11:16:07 +0100

    linux test

--- .vib/linux-install/scripts/tests.sh
+++ .vib/linux-install/scripts/tests.sh
@@ -1,3 +1,27 @@
-#!/bin/bash -ex
+#!/bin/bash
 
-find . -ls
+DEB_FILE=$(find . -name "wavefront-proxy*deb")
+JAR_FILE="/opt/wavefront/wavefront-proxy/bin/wavefront-proxy.jar"
+
+if [ -f "${DEB_FILE}" ]; then
+    echo "${DEB_FILE} exists."
+else 
+    echo "${DEB_FILE} does not exist."
+    exit 100
+fi
+
+dpkg -i ${DEB_FILE}
+retVal=$?
+if [ ${retVal} -ne 0 ]; then
+    echo "dpkg Error "${retVal}
+    exit 101
+fi
+
+if [ -f "${JAR_FILE}" ]; then
+    echo "${JAR_FILE} exists."
+else 
+    echo "${JAR_FILE} does not exist."
+    exit 102
+fi
+
+exit 0
