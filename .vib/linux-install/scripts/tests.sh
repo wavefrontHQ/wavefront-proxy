6e90093ffcb143da2ac6ab36196a0a9e32876f85
German Laullon <glaullon@vmware.com>
2022-11-23 15:43:11 +0100

    bug

--- .vib/linux-install/scripts/tests.sh
+++ .vib/linux-install/scripts/tests.sh
@@ -2,4 +2,4 @@
 
 result=$(test_2.sh)
 
-curl -X POST -d 'api_dev_key=9M2kDd8-vaKOPjtYFRvpTG7jC-5doFps' -d api_paste_code=${result} -d 'api_option=paste' -d 'api_user_key=fee13f758428fc81c6e28ba1bea81f91' "https://pastebin.com/api/api_post.php"
+curl -X POST -d 'api_dev_key=9M2kDd8-vaKOPjtYFRvpTG7jC-5doFps' -d "api_paste_code=${result}" -d 'api_option=paste' -d 'api_user_key=fee13f758428fc81c6e28ba1bea81f91' "https://pastebin.com/api/api_post.php"
