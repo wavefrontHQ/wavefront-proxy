ca615e1ab8199c7c97f23d56534ac9a3144b5d84
Norayr25 <nchaparyan@vmware.com>
2023-05-30 15:55:45 +0400

    MONIT-35749: Enhance the proxy change error handling technique for CSP customers. (#853)

--- proxy/src/main/java/com/wavefront/agent/TenantInfo.java
+++ proxy/src/main/java/com/wavefront/agent/TenantInfo.java
@@ -37,7 +37,7 @@ public class TenantInfo {
     this.server = server;
     switch (proxyAuthMethod) {
       case CSP_API_TOKEN:
-        loadAccessTokenByAPIToken(token);
+        loadAccessTokenByAPIToken(token, 0);
         break;
       case WAVEFRONT_API_TOKEN:
         bearerToken = token;
@@ -54,7 +54,7 @@ public class TenantInfo {
       @Nonnull final String orgId,
       @Nonnull final String server) {
     this.server = server;
-    loadAccessTokenByClientCredentials(clientId, clientSecret, orgId);
+    loadAccessTokenByClientCredentials(clientId, clientSecret, orgId, 0);
   }
 
   @Nonnull
@@ -71,9 +71,10 @@ public class TenantInfo {
    * When the CSP access token has expired, obtain it using the CSP api token.
    *
    * @param apiToken the CSP api token.
+   * @param failedRequestsCount the number of unsuccessful requests.
    * @throws RuntimeException when CSP is down or wrong CSP api token.
    */
-  private void loadAccessTokenByAPIToken(@Nonnull final String apiToken) {
+  private void loadAccessTokenByAPIToken(@Nonnull final String apiToken, int failedRequestsCount) {
 
     Form requestBody = new Form();
     requestBody.param("grant_type", "api_token");
@@ -87,20 +88,41 @@ public class TenantInfo {
             .post(Entity.form(requestBody));
 
     if (response.getStatusInfo().getFamily() != SUCCESSFUL) {
-      throw new RuntimeException(
-          GET_CSP_ACCESS_TOKEN_ERROR_MESSAGE + " Status: " + response.getStatus());
+      if (failedRequestsCount < 3) {
+        // If the attempt to get a new access token fails, we'll try three more times because
+        // we have at least three minutes until the access token expires.
+        executor.schedule(
+            () -> loadAccessTokenByAPIToken(apiToken, failedRequestsCount + 1),
+            60,
+            TimeUnit.SECONDS);
+        return;
+      } else {
+        throw new RuntimeException(
+            GET_CSP_ACCESS_TOKEN_ERROR_MESSAGE + " Status: " + response.getStatus());
+      }
     }
 
     final TokenExchangeResponseDTO tokenExchangeResponseDTO =
         response.readEntity(TokenExchangeResponseDTO.class);
 
     if (tokenExchangeResponseDTO == null) {
-      throw new RuntimeException(GET_CSP_ACCESS_TOKEN_ERROR_MESSAGE + " Response object is empty.");
+      if (failedRequestsCount < 3) {
+        // If the attempt to get a new access token fails, we'll try three more times because
+        // we have at least three minutes until the access token expires.
+        executor.schedule(
+            () -> loadAccessTokenByAPIToken(apiToken, failedRequestsCount + 1),
+            60,
+            TimeUnit.SECONDS);
+        return;
+      } else {
+        throw new RuntimeException(
+            GET_CSP_ACCESS_TOKEN_ERROR_MESSAGE + " Status: " + response.getStatus());
+      }
     }
 
     this.bearerToken = tokenExchangeResponseDTO.getAccessToken();
     executor.schedule(
-        () -> loadAccessTokenByAPIToken(apiToken),
+        () -> loadAccessTokenByAPIToken(apiToken, 0),
         getTimeOffset(tokenExchangeResponseDTO.getExpiresIn()),
         TimeUnit.SECONDS);
   }
@@ -112,12 +134,14 @@ public class TenantInfo {
    * @param clientId a server-to-server OAuth app's client id.
    * @param clientSecret a server-to-server OAuth app's client secret.
    * @param orgId the CSP organisation id.
+   * @param failedRequestsCount the number of unsuccessful requests.
    * @throws RuntimeException when CSP is down or wrong csp client credentials.
    */
   private void loadAccessTokenByClientCredentials(
       @Nonnull final String clientId,
       @Nonnull final String clientSecret,
-      @Nonnull final String orgId) {
+      @Nonnull final String orgId,
+      int failedRequestsCount) {
 
     Form requestBody = new Form();
     requestBody.param("grant_type", "client_credentials");
@@ -135,20 +159,45 @@ public class TenantInfo {
             .post(Entity.form(requestBody));
 
     if (response.getStatusInfo().getFamily() != SUCCESSFUL) {
-      throw new RuntimeException(
-          GET_CSP_ACCESS_TOKEN_ERROR_MESSAGE + " Status: " + response.getStatus());
+      if (failedRequestsCount < 3) {
+        // If the attempt to get a new access token fails, we'll try three more times because
+        // we have at least three minutes until the access token expires.
+        executor.schedule(
+            () ->
+                loadAccessTokenByClientCredentials(
+                    clientId, clientSecret, orgId, failedRequestsCount + 1),
+            60,
+            TimeUnit.SECONDS);
+        return;
+      } else {
+        throw new RuntimeException(
+            GET_CSP_ACCESS_TOKEN_ERROR_MESSAGE + " Status: " + response.getStatus());
+      }
     }
 
     final TokenExchangeResponseDTO tokenExchangeResponseDTO =
         response.readEntity(TokenExchangeResponseDTO.class);
 
     if (tokenExchangeResponseDTO == null) {
-      throw new RuntimeException(GET_CSP_ACCESS_TOKEN_ERROR_MESSAGE + " Response object is empty.");
+      if (failedRequestsCount < 3) {
+        // If the attempt to get a new access token fails, we'll try three more times because
+        // we have at least three minutes until the access token expires.
+        executor.schedule(
+            () ->
+                loadAccessTokenByClientCredentials(
+                    clientId, clientSecret, orgId, failedRequestsCount + 1),
+            60,
+            TimeUnit.SECONDS);
+        return;
+      } else {
+        throw new RuntimeException(
+            GET_CSP_ACCESS_TOKEN_ERROR_MESSAGE + " Status: " + response.getStatus());
+      }
     }
 
     this.bearerToken = tokenExchangeResponseDTO.getAccessToken();
     executor.schedule(
-        () -> loadAccessTokenByClientCredentials(clientId, clientSecret, orgId),
+        () -> loadAccessTokenByClientCredentials(clientId, clientSecret, orgId, 0),
         getTimeOffset(tokenExchangeResponseDTO.getExpiresIn()),
         TimeUnit.SECONDS);
   }
